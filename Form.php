e6e9288ee10f56c35457d22a96fea9b235a28a00
Nisma <67649383+nisma687@users.noreply.github.com>
2022-02-02 09:25:46 +0600

    Add files via upload

---
+++ Form.php
@@ -0,0 +1,51 @@
+<!doctype html>
+
+<html>
+<head>
+    
+</head>
+
+<body>
+    <h2> Registration Form </h2>
+<form>
+    
+    <label> First Name : </label>
+    <input type="text" value="Enter your name"><br>
+    <label> Last Name : </label>
+    <input type="text" value="Enter your  last name"><br>
+    <label> Age : </label>
+    <input type="text" ><br>
+    <label> Designation : 
+    <input type="radio" name="designation" value="designation">Junior Programmer
+
+    <input type="radio" name="designation" value="designation">Senior Programmer
+   
+    <input type="radio" name="designation" value="designation">Project Lead<br>
+    </label>
+    <label>Preferred Language:
+        <input type="checkbox">Java
+        <input type="checkbox">PHP
+        <input type="checkbox">C++ <br>
+</label>
+<label>E-mail:
+    <input type="text" ><br>
+</label>
+<label>Password:
+    <input type="text">
+</label>
+<p>Please choose a file  <input type="submit" value="Choose File">  </p>
+</form>
+</body>
+
+
+
+
+
+
+
+
+
+
+
+
+</html>
